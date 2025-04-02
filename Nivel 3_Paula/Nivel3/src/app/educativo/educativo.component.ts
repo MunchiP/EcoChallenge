bb1e3250c93aa84042d9aae4a5ca81cb9b0f37c6
MunchiP <paulaviveusa@gmail.com>
2025-04-01 21:49:34 -0500

    Creación del proyecto del Nivel 3

---
+++ Nivel 3_Paula/Nivel3/src/app/educativo/educativo.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-educativo',
+  imports: [],
+  templateUrl: './educativo.component.html',
+  styleUrl: './educativo.component.css'
+})
+export class EducativoComponent {
+
+}
