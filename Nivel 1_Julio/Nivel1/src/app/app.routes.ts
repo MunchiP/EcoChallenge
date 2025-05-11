edd76b82dd3167f380352ef8d92a6ecc9da79013
JulioRMon <julio.ramirez02@uptc.edu.co>
2025-05-11 00:17:06 -0500

    se realiza enrutamiento pantallas

--- Nivel 1_Julio/Nivel1/src/app/app.routes.ts
+++ Nivel 1_Julio/Nivel1/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Educativo1Component } from './educativo1/educativo1.component';
 import { Juego1component } from './juego1/juego1.component';
 import { EducativoComponent } from './../../../../Nivel 2_Andres/Nivel2/src/app/educativo/educativo.component';
+import {CaminoReciclajeComponent} from './../../../../Nivel 2_Andres/Nivel2/src/app/camino-reciclaje/camino-reciclaje.component';
 export const routes: Routes = [
   { path: 'educativo1', component: Educativo1Component },
   { path: 'juego1', component: Juego1component },
   { path: 'educativo2', component: EducativoComponent},
+  { path: 'camino-reciclaje', component: CaminoReciclajeComponent},
 
    { path: '', redirectTo: '/educativo1', pathMatch: 'full' } // Ruta por defecto
 ];
